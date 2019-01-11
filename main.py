7260dedc71a321ea51054a7bc944ba948f4d7e0c
Matej Majtan <majtan.matej@gmail.com>
2019-01-11 10:56:03 +0100

    updated pinout and added RGB and extra button

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import time
 
 # configuration of the pins
 sensor = ADC(Pin(34))
-led = PWM(Pin(13))
-button = Pin(12, Pin.IN, Pin.PULL_UP)
+led = PWM(Pin(13), freq=1000)
+red = PWM(Pin(14), freq=1000)
+blue = PWM(Pin(32), freq=1000)
+green = PWM(Pin(15), freq=1000)
+mode_btn = Pin(22, Pin.IN, Pin.PULL_UP) # closer button
+action_btn = Pin(23, Pin.IN, Pin.PULL_UP) # further button
+
 time_c = 0
 itterator = 0
 
@@ -18,14 +23,14 @@ f.write("Day is always the first!!!\n")
 f.close()
 
 def collect():
-    led.duty(1000)
+    led.duty(500)
     time.sleep(0.01)
     data = []
     for i in range(8): 
         data.append(sensor.read())
     data = str(sum(data)/8)
     f = open("data.txt", "a+")
-    f.write(str(itterator)+" - "+str(data)+"\n")
+    f.write(str(itterator)+" - "+data+"\n")
     f.close()
     time.sleep(0.01)
     led.duty(0)
@@ -35,7 +40,7 @@ while True:
     if time_c >= 100: #TODO: was 6000
         collect()
         itterator += 1
-    if button.value() == 0: 
+    if mode_btn.value() == 0: 
         break 
     time.sleep(0.01)
     time_c += 1
