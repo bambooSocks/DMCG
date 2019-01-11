fab54b8e3bcdf1dcef4ce760fbf9ceec8195f897
Matej Majtan <majtan.matej@gmail.com>
2019-01-11 13:42:05 +0100

    added state machine

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ mode_btn = Pin(22, Pin.IN, Pin.PULL_UP) # closer button
 action_btn = Pin(23, Pin.IN, Pin.PULL_UP) # further button
 
 time_c = 0
-itterator = 0
 
 sensor.atten(3)     # ATTN_11DB
 led.freq(1000)
@@ -26,22 +25,55 @@ def collect():
     led.duty(500)
     time.sleep(0.01)
     data = []
-    for i in range(8): 
+    for i in range(100): 
         data.append(sensor.read())
-    data = str(sum(data)/8)
     f = open("data.txt", "a+")
-    f.write(str(itterator)+" - "+data+"\n")
+    f.write(str(sum(data)/100)+"\n")
     f.close()
     time.sleep(0.01)
     led.duty(0)
     time_c = 0
 
+mode = 0 # 0 - disabled, 1 - reference, 2 - measurement
+meas_enabled = False
+
 while True:
-    if time_c >= 100: #TODO: was 6000
-        collect()
-        itterator += 1
-    if mode_btn.value() == 0: 
-        break 
-    time.sleep(0.01)
-    time_c += 1
-    print("I am running")
+    if mode == 0:
+        if mode_btn.value() == 0:
+            mode = 1
+            continue
+        if action_btn.value() == 0:
+            pass
+            #nothing
+        # LED red solid
+
+    # reference setup mode
+    elif mode == 1:
+        if mode_btn.value() == 0:
+            mode = 2
+            continue
+        if action_btn.value() == 0:
+            reference()
+            # LED blue blinking
+        # LED blue solid
+
+    # measurement mode
+    elif mode == 2:
+        if mode_btn.value() == 0:
+            mode = 0
+            meas_enabled = False
+            time_c = 0
+            continue
+        if action_btn.value() == 0:
+            meas_enabled = True
+        if meas_enabled:
+            if time_c >= 1000: #TODO: was 60000
+                collect()
+            time_c += 1
+            # LED green blinking
+        # LED green solid
+
+    else:
+        mode = 0
+
+    time.sleep(0.001)
